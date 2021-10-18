319742c63c224d67588b05fdb8a1f23751072424
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2021-10-18 21:57:26 +0530

    Update checkCharacter.java

--- 3. Conditionals/checkCharacter.java
+++ 3. Conditionals/checkCharacter.java
@@ -1,1 +1,23 @@
+// InterviewBit online compiler
 
+import java.util.Scanner;
+
+class Main {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		char c = s.next().charAt(0);
+	
+		if(c >= 'A' && c <= 'Z'){
+			System.out.println("1");
+		} 
+		else if(c>='a' && c <= 'z'){
+			System.out.println("0");
+		}
+		else{
+			System.out.println("-1");
+		}
+
+		System.out.println(c);
+
+	}
+}
