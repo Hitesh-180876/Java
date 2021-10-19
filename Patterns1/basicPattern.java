8b12b50f332d9dc39f3011e5ab35f9ada4b30d23
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2021-10-19 17:09:00 +0530

    Update basicPattern.java

--- Patterns1/basicPattern.java
+++ Patterns1/basicPattern.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+class Main {
+	public static void main(String args[]) {
+		// Your code goes here
+
+		Scanner s = new Scanner(System.in);
+		int N;
+		N = s.nextInt();
+
+		int i = 1;
+		while(i<=N){
+			int j = 1;
+			while(j<=N){
+				System.out.print("*");
+				j+=1;
+			}
+			System.out.println("");
+			i+=1;
+		}
+	}
+}
