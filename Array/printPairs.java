99da9afccfe32f0449d61a6bd5d4476cbde7d1c3
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2021-10-25 17:34:52 +0530

    Update printPairs.java

--- Array/printPairs.java
+++ Array/printPairs.java
@@ -1,1 +1,17 @@
+// online jdoodle compiler
 
+public class MyClass {
+    
+    public static void printArray(int[] array){
+        for(int i=0; i<array.length-1; i++){
+            for(int j=i+1;j<array.length;j++){
+                System.out.print("("+array[i]+","+array[j]+")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        int[] arr = {1, 3, 5, 10};
+        printArray(arr);
+    }
+}
